7c46625314eda955845a28353b7276d59ab78a66
Chris Aquino <chris.aquino@gmail.com>
2018-12-10 10:28:39 -0500

    adds basic routing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router, // Watches for changes to URL
+  Route,                   // Renders components based on the specific URL
+  Link                     // Changes the URL so the Router sees it
+} from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+import Cats from './Cats';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/cats">Cats</Link></li>
+          </ul>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cats" component={Cats} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Cats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cats = (props) => {
+  return (
+    <div>
+        <h1>CAAAAATSSSz!</h1>
+    </div>
+  );
+};
+
+export default Cats;
