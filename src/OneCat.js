1a85467e3d4c3569d1df97a979d4397a54cb35c6
Chris Aquino <chris.aquino@gmail.com>
2018-12-10 12:07:46 -0500

    dynamically draws cat toys to page

--- src/OneCat.js
+++ src/OneCat.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const OneCat = (props) => {
+    const allToys = props.toys;
+    const catName = props.match.params.catName;
+    const myToys = allToys[catName];
     return (
-        <h2>
-            {props.match.params.catName}
-        </h2>
+        <div>
+            <h2>
+                {catName}
+            </h2>
+            <ul>
+                {myToys.map((toy) => {
+                    return <li>{toy}</li>
+                })}
+            </ul>
+        </div>
     );
 };
 
