2bc6a03c7afe9961b12e5f6d55fe54ee7f427f0c
Chris Aquino <chris.aquino@gmail.com>
2018-12-14 10:25:14 -0500

    ready for production\!

---
+++ build/precache-manifest.b09dc2d28c586ab9a43481c6a5e178a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40c053b1469893716537",
+    "url": "/static/js/main.40c053b1.chunk.js"
+  },
+  {
+    "revision": "38f5b6b4015128c0bd1e",
+    "url": "/static/js/1.38f5b6b4.chunk.js"
+  },
+  {
+    "revision": "40c053b1469893716537",
+    "url": "/static/css/main.8e6db31b.chunk.css"
+  },
+  {
+    "revision": "eda5e232775fa4ae526655adcdaec7b4",
+    "url": "/index.html"
+  }
+];
